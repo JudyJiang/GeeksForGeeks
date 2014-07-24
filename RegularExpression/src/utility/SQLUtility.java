e2133bbe92dc08a3daa870d2ce60bb1d018ae04e
Fengjiao <fj2194@columbia.edu>
2014-07-24 15:36:30 -0400

    Writing code during meeting is horrible

--- RegularExpression/src/utility/SQLUtility.java
+++ RegularExpression/src/utility/SQLUtility.java
@@ -5,6 +5,43 @@ import java.util.*;
 
 
 public class SQLUtility {
+	
+	public static List<String> readTextFile(String SQLFilePath) 
+			throws IOException{
+		List<String> list = new ArrayList<String>();
+		File file = new File(SQLFilePath);
+		
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		
+		String line;
+		String oneResultLine;
+		while((line = br.readLine()) != null){
+			oneResultLine = "";
+			/**
+			 * Time processing
+			 * 1. First tokenize line by line;
+			 * 2. Then tokenize one line by ",";
+			 * 3. For now just split by "//" but in the future use regex
+			 * 
+			 */
+			
+			String[] dataEntries = line.split(",");
+			for(String dataEntry : dataEntries){
+				if(dataEntry.contains("")){
+					
+				}
+				
+				if(){
+					
+				}
+			}
+			
+			list.add(oneResultLine);
+		}
+		
+		return list;
+	}
+	
 	public static void main(String[] args){
 		
 	}

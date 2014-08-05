7264fefa92f370c45ef545cd871fa02d25cfd124
Fengjiao <fj2194@columbia.edu>
2014-08-05 16:36:33 -0400

    Add little program to count words that have the same anagram

---
+++ RegularExpression/src/Tree/TreeCousin.java
@@ -0,0 +1,23 @@
+package Tree;
+
+/**
+ * Two nodes are cousin within a binary tree if:
+ * 1. They're on the same level;
+ * 2. They have different parents.
+ * @author FengjiaoJiang
+ *
+ */
+public class TreeCousin {
+	public static class AncestorWithDepth{
+		BinaryTree ancestor;
+		int depth;
+	}
+	
+	public static boolean IsCousin(BinaryTree root, BinaryTree p, BinaryTree q){
+		return true;
+	}
+	public static AncestorWithDepth(BinaryTree root, BinaryTree p, BinaryTree q){
+		
+	}
+	///public static void 
+}

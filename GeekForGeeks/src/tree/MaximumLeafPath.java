914f803d1565e25eaa22af2aaf3bddc1f9301355
Fengjiao <fj2194@columbia.edu>
2014-08-13 11:27:02 -0400

    Today's practise, just started

---
+++ GeekForGeeks/src/tree/MaximumLeafPath.java
@@ -0,0 +1,23 @@
+package tree;
+
+/**
+ * Find Maximum Sum path within two leaves.
+ * Doesn't have to through the root.
+ * 
+ * http://www.geeksforgeeks.org/find-maximum-path-sum-two-leaves-binary-tree/
+ * 
+ * @author FengjiaoJiang
+ *
+ */
+
+public class MaximumLeafPath {
+	public static BinaryTree<Integer> ConstructSampleTree(){
+		return null;
+	}
+	
+	public static int MaximumSum(BinaryTree<Integer> root){
+		int sum = 0;
+		return sum;
+	}
+	//MaxLeft, MaxRight, root.value (check if added root.value is any larger)
+}

2f6cceb3284de4b56b733276ecde3dc73ef69964
Fengjiao <fj2194@columbia.edu>
2014-08-19 11:23:28 -0400

    Adding new staff

--- GeekForGeeks/src/linkedlist/SwapPairNode.java
+++ GeekForGeeks/src/linkedlist/SwapPairNode.java
@@ -5,29 +5,39 @@ package linkedlist;
  * The original structure is still there, the node is actually "runner pointer" that moves around 
  * the LinkedList nodes. 
  * @author FengjiaoJiang
+ * 
+ * 
+ * 
+ * 1->2->3->4->5->6->null
  *
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class SwapPairNode {
 	public static ListNode<String> SwapPairWiseNode(ListNode<String> list){
 		if(list == null || list.next == null)
 			return list;
-		
 		ListNode<String> head = new ListNode<String>();
-		head.next = list;
-		
-		ListNode<String> prev = list;
-		ListNode<String> next = list.next;
-		
-		while(prev != null){
-			ListNode<String> temp = prev.next.next;
-			prev.next = next.next;
-			next.next = prev;
+
+		ListNode prev = list;
+		ListNode curr = list.next;
+
+		while(){
 			
-			prev = temp;
-			next = prev == null ? null : prev.next;
 		}
-		
-		return head.next;
+		return null;
 	}
 	
 	public static void main(String[] args){

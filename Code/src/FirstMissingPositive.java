afd673d68fdf8a805850c9f65b4be0c95f57696a
Fengjiao Jiang <GrapeJudy@gmail.com>
2015-06-17 06:07:31 -0400

    two problems to be solved

---
+++ Code/src/FirstMissingPositive.java
@@ -0,0 +1,10 @@
+
+public class FirstMissingPositive {
+	public static int firstMissingPositive(int[] nums){
+		if(nums == null || nums.length == 0)
+			return -1;
+		
+		int i = 0, j = nums.length;
+		return -1;
+	}
+}

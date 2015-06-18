96470081746af57e0bda876e7b2f551e563df996
Fengjiao Jiang <GrapeJudy@gmail.com>
2015-06-17 23:17:33 -0400

    solved two for tomorrow

--- Code/src/ConvertSortedArraytoBST.java
+++ Code/src/ConvertSortedArraytoBST.java
@@ -1,4 +1,23 @@
+import utility.*;
+
 
 public class ConvertSortedArraytoBST {
-
+	public static TreeNode sortedArrayToBST(int[] nums){
+		if(nums == null || nums.length == 0)
+			return null;
+		
+		return sortedArrayToBST(nums, 0, nums.length);
+	}
+	
+	public static TreeNode sortedArrayToBST(int[] nums, int left, int right){
+		if(left >= right)
+			return null;
+		
+		int mid = (left + right) / 2;
+		TreeNode root = new TreeNode(nums[mid]);
+		
+		root.left = sortedArrayToBST(nums, left, mid);
+		root.right = sortedArrayToBST(nums, mid + 1, right);
+		return root;
+	}
 }

d03b950bcecb42b6901292a801c1b0f99f334700
JudyJiang <fj2194@columbia.edu>
2015-06-18 11:53:41 -0400

    finally accepted basic calcultor

--- Code/src/BasicCalculator.java
+++ Code/src/BasicCalculator.java
@@ -1,5 +1,11 @@
 import java.util.*;
 
+/**
+ * Finally accepted....
+ * Need really to be paid attention
+ * @author FengjiaoJiang
+ *
+ */
 public class BasicCalculator {
 	public static int calculate(String s) {
 		//use two stacks
@@ -8,17 +14,33 @@ public class BasicCalculator {
 		Stack<String> s1 = new Stack<String>();
 		Stack<String> s2 = new Stack<String>();
 		
+		
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < s.length(); i++){
 			char tmp = s.charAt(i);
 			if(tmp != ' '){
 				if(tmp == ')'){
+					if(!sb.toString().equals("")){
+						s1.push(sb.toString());
+						sb = new StringBuilder();
+					}
 					calculate(s1, s2);
 				}
 				else{
-					s1.push(String.valueOf(tmp));
+					if(tmp == '+' || tmp == '-' || tmp == '('){
+						if(!sb.toString().equals(""))
+								s1.push(sb.toString());
+						s1.push(String.valueOf(tmp));
+						sb = new StringBuilder();
+					}
+					else
+						sb.append(tmp);
 				}
 			}
 		}
+		
+		if(!sb.toString().equals(""))
+			s1.push(sb.toString());
 		
 		if(s1.size() > 1)
 			calculate(s1, s2);
@@ -30,21 +52,22 @@ public class BasicCalculator {
 	public static void calculate(Stack<String> s1, Stack<String> s2){
 		while(!s1.isEmpty() && !s1.peek().equals("(")){
 			s2.push(s1.pop());
+			
 		}
 		
 		if(!s1.isEmpty() && s1.peek().equals("("))
 			s1.pop();
-		
-		
+	
 		while(!s2.isEmpty()){
 			String opr1 = s2.pop();
 			if(s2.isEmpty()){
 				s1.push(opr1);
 				return ;
 			}
+			
 			String op = s2.pop();
 			String opr2 = s2.pop();
-			//System.out.println(opr1 + " " + opr2);
+
 			String res = op.equals("+") ? String.valueOf(Integer.valueOf(opr1) + Integer.valueOf(opr2)) : 
 				                          String.valueOf(Integer.valueOf(opr1) - Integer.valueOf(opr2));
 			s2.push(res);
@@ -53,7 +76,7 @@ public class BasicCalculator {
 
 
 	public static void main(String[] args) {
-		String s = "(2)+(2)+(2+3)+(4-1)-(1)";
+		String s = "(1122) + (334) - (((12)) -4)";
 		int res = calculate(s);
 		System.out.println(res);
 	}

ed95271fb3bcf56249af86a6fea6eb562df84f7c
Fengjiao Jiang <GrapeJudy@gmail.com>
2015-07-09 09:59:51 -0400

    compute

--- Code/src/careercup/Computation.java
+++ Code/src/careercup/Computation.java
@@ -6,40 +6,68 @@ public class Computation {
 	private static String[] operands = {"+", "-", "*", "/"};
 	
 	
-	public static List<int[]> permutations(int[] nums){
-		return null;
+	public static List<List<Integer>> permutations(int[] nums){
+		List<List<Integer>> res = new LinkedList<List<Integer>>();
+		boolean[] visited = new boolean[nums.length];
+		List<Integer> list = new LinkedList<Integer>();
+		permutations(nums, res, list, visited);
+		return res;
+	}
+	
+	public static void permutations(int[] nums, List<List<Integer>> res, List<Integer> list, boolean[] visited){
+		if(list.size() == nums.length){
+			List<Integer> copy = new LinkedList<Integer>();
+			copy.addAll(list);
+			res.add(copy);
+			return ;
+		}
+		
+		for(int i = 0; i < nums.length; i++){
+			if(!visited[i]){
+				list.add(nums[i]);
+				visited[i] = true;
+				permutations(nums, res, list, visited);
+				visited[i] = false;
+				list.remove(list.size() - 1);
+			}
+		}
 	}
 	
 	
 	public static String evaluate(int[] nums, int target){
-		List<int[]> perms = permutations(nums);
+		List<List<Integer>> perms = permutations(nums);
 		Stack<Integer> s = new Stack<Integer>();
 		List<List<String>> trace = new LinkedList<List<String>>();
 		List<String> expression = new LinkedList<String>();
 	
-		for(int[] list : perms){
+		for(List<Integer> list : perms){
 			for(int i : list)
 				s.push(i);
 			evaluate(s, target, trace);
+			s = new Stack<Integer>();
 		}
 		return "";
 	}
 	
 	public static void evaluate(Stack<Integer> s, int target, List<List<String>> trace){
+		if(s.size() == 0)
+			return;
 		if(s.size() == 1 && s.peek() == target){
+			System.out.println("Yes, here");
 			return ;
 		}
 		
 		if(s.size() == 1 && s.peek() != target){
-			
+			return ;
 		}
 		
-		int a = s.pop();
-		int b = s.pop();
+		int a = s.pop(), b = s.pop();
 		for(int i = 0; i < operands.length; i++){
+			System.out.println(a + " " + b + compute(a, b, operands[i]));
 			s.push(compute(a, b, operands[i]));
 			evaluate(s, target, trace);
-			s.pop();
+			if(!s.isEmpty())
+				s.pop();
 		}
 	}
 	
@@ -64,6 +92,8 @@ public class Computation {
 	}
 	
 	public static void main(String[] args){
-		System.out.println("I hate life");
+		//2 + 3 - 1 * 6
+		int[] nums = {2, 3, 1, 6};
+		evaluate(nums, 24);
 	}
 }
--- Code/src/graph/LongestPath.java
+++ Code/src/graph/LongestPath.java
@@ -30,7 +30,36 @@ public class LongestPath {
 			this.adj.get(v).add(new GraphNode(w, weight));
 		}
 		
+		public void topologicalSort(Stack<Integer> s, boolean[] visited){
+			
+		}
+		
 		public void longestPath(int start){
+			Stack<Integer> topoS = new Stack<Integer>();
+			boolean[] visited = new boolean[this.V];
+			topologicalSort(topoS, visited);
+			int[] dist = new int[this.V];
+			for(int i = 0; i < this.V; i++){
+				dist[i] = Integer.MIN_VALUE;
+			}
+			dist[start] = 0;
+			while(!topoS.isEmpty()){
+				int top = topoS.pop();
+				if(dist[top] != Integer.MIN_VALUE){
+					for(GraphNode i : this.adj.get(top)){
+						if(dist[i.v] < dist[top] + i.weight)
+							dist[i.v] = dist[top] + i.weight;
+					}
+				}
+			}
+			
+			for(int i = 0; i < this.V; i++){
+				System.out.print(dist[i] + " ");
+			}
+		}
+	}
+		
+/*		public void longestPath(int start){
 			HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 			boolean[] visited = new boolean[this.V];
 			for(int i = 0; i < V; i++){
@@ -38,6 +67,10 @@ public class LongestPath {
 			}
 			map.put(start, 0);
 			longestPath(start, 0, map, visited);
+			Iterator it = map.entrySet().iterator();
+			while(it.hasNext()){
+				System.out.println(it.next());
+			}
 		}
 		
 		public void longestPath(int node, int value, HashMap<Integer, Integer> map, boolean[] visited){
@@ -52,5 +85,12 @@ public class LongestPath {
 				}
 			}
 		}
+	} */
+	//DFS with previous node value doesn't work. cause if not sorted in topological order,
+		//some visited node may not be considered to be visited again but they're actually 
+		//part of the longest path
+	
+	public static void main(String[] args){
+		System.out.println("Yes");
 	}
 }
